fd4bd507e5deafad69f8537f2fd8f456c4feef8e
Samuele <samuele.coppede@gmail.com>
2026-01-20 22:29:44 +0100

    remove Border node

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
-from .nodes import VPypeProcessor, VPypeGCodeGenerator, VPypeRemoveBorder
+from .nodes import VPypeProcessor, VPypeGCodeGenerator
 
 __version__ = "1.0.0"
 __author__ = "Samuele"
@@ -6,14 +6,12 @@ __description__ = "ComfyUI nodes for processing SVG and generating G-code using 
 
 NODE_CLASS_MAPPINGS = {
     "VPypeProcessor": VPypeProcessor,
-    "VPypeGCodeGenerator": VPypeGCodeGenerator,
-    "VPypeRemoveBorder": VPypeRemoveBorder
+    "VPypeGCodeGenerator": VPypeGCodeGenerator
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
     "VPypeProcessor": "VPype SVG Processor",
-    "VPypeGCodeGenerator": "VPype G-Code Generator",
-    "VPypeRemoveBorder": "VPype Remove Border"
+    "VPypeGCodeGenerator": "VPype G-Code Generator"
 }
 
 
