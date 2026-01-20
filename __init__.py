701f915c2da7c5e821777e61efc8bdf4cf23f7a2
Samuele <samuele.coppede@gmail.com>
2026-01-20 20:40:36 +0100

    gcode

--- __init__.py
+++ __init__.py
@@ -1,11 +1,14 @@
-from .nodes import VPypeProcessor
+from .nodes import VPypeProcessor, VPypeGCodeGenerator
 
 NODE_CLASS_MAPPINGS = {
-    "VPypeProcessor": VPypeProcessor
+    "VPypeProcessor": VPypeProcessor,
+    "VPypeGCodeGenerator": VPypeGCodeGenerator
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "VPypeProcessor": "VPype SVG Processor"
+    "VPypeProcessor": "VPype SVG Processor",
+    "VPypeGCodeGenerator": "VPype G-Code Generator"
 }
+
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
